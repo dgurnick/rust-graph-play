e2b342f2234258cd3bae54c8c5aa4db8af34fdab
Dennis Gurnick <dennis.gurnick@klueger-digital.com>
2020-06-11 16:34:40 +0200

    Project Skeleton

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rust-graph-play
 Playground for graphql in Rust. 
+
+# References
+
+1. https://github.com/joshua-cooper/rust-graphql-intro
+1. https://www.lpalmieri.com/posts/2020-05-10-announcement-zero-to-production-in-rust/
+
+# Getting started
+docker run --rm -it -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres:alpine
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use warp::Filter;
+use std::sync::Arc;
+use juniper::http::graphiql::graphiql_source;
+use juniper::RootNode;
+use tokio_postgres::Client;
+use std::convert::Infallible;
+use juniper::http::GraphQLRequest;
+use tokio_postgres::NoTls;
+
+struct QueryRoot;
+struct MutationRoot;
+
+#[juniper::graphql_object(Context = Context)]
+impl QueryRoot {}
+
+#[juniper::graphql_object(Context = Context)]
+impl MutationRoot {}
+
+type Schema = RootNode<'static, QueryRoot, MutationRoot>;
+
+struct Context {
+    client: Client,
+}
+
+#[tokio::main]
+async fn main() {
+
+    // Connect to Postgres
+    let (client, connection) = tokio_postgres::connect("host=localhost user=postgres", NoTls)
+        .await
+        .unwrap();
+
+    // Let the connection run on its own
+    tokio::spawn(async move {
+       if let Err(e) = connection.await {
+           eprintln!("Database connection error: {}", e);
+       }
+    });
+
+    client.execute(
+        "CREATE TABLE IF NOT EXISTS customers ( \
+        id UUID PRIMARY KEY, \
+        name TEXT NOT NULL, \
+        age INT NOT NULL, \
+        email TEXT UNIQUE NOT NULL, \
+        address TEXT NOT NULL \
+        )",
+        &[],
+        )
+        .await
+        .expect("Could not create customers table");
+
+
+    // Define the schema
+    let schema = Arc::new(Schema::new(QueryRoot, MutationRoot));
+
+    // Make schema into a warp filter so that it is reachable via route handlers
+    let schema = warp::any().map(move || Arc::clone(&schema));
+
+    // Set up a context so we can do things like DB, etc
+    let ctx = Arc::new(Context { client });
+
+    // Make the context available to warp routes
+    let ctx = warp::any().map(move || Arc::clone(&ctx));
+
+    // Set up the graphql query handler
+    let graphql_route = warp::post()
+        .and(warp::path!("graphql"))
+        .and(schema.clone())
+        .and(ctx.clone())
+        .and(warp::body::json())
+        .and_then(graphql);
+
+    // Set up the graph response (contract)
+    let graphiql_route = warp::get()
+        .and(warp::path("graphiql"))
+        .map(|| warp::reply::html(graphiql_source("graphql")));
+
+    // Combine our routes
+    let routes = graphql_route.or(graphiql_route);
+
+    // Start listening for requests
+    warp::serve(routes).run(([127,0,0,1], 8000)).await;
+
+}
+
+async fn graphql (
+    schema: Arc<Schema>,
+    ctx: Arc<Context>,
+    req: GraphQLRequest
+) -> Result<impl warp::Reply, Infallible> {
+    let res = req.execute_async(&schema, &ctx).await;
+    let json = serde_json::to_string(&res)
+        .expect("Invalid JSON response");
+    Ok(json)
+}
